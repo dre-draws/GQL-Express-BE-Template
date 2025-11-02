581740cc9227a9a61254030c0cb37264f5328691
Adriano Triana <dredrawzz@gmail.com>
2025-11-02 23:26:27 +0100

    Hooking up railway env variables

--- src/datasource.ts
+++ src/datasource.ts
@@ -3,14 +3,14 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "test",
-  password: "test",
-  database: "test",
+  host: process.env.PG_HOST,
+  port: Number(process.env.PG_PORT),
+  username: process.env.PG_USERNAME,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DB,
   synchronize: true,
   logging: false,
   entities: [path.resolve(__dirname) + "/entities/**/*.{js,ts}"],
   migrations: [path.resolve(__dirname) + "/migrations/*.{js,ts}"],
-  subscribers: [path.resolve(__dirname) + "/subcribers/**/*.{js,ts}"],
+  subscribers: [path.resolve(__dirname) + "/subscribers/**/*.{js,ts}"],
 });
