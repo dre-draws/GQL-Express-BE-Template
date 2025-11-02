9fb767bb52ef31563129f16edc5b375a226c75e5
Adriano Triana <dredrawzz@gmail.com>
2025-11-02 22:41:30 +0100

    updating express and stripping out index file

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,14 @@
-import bodyParser from "body-parser";
 import express from "express";
-import pg from "pg";
-
-// Connect to the database using the DATABASE_URL environment
-//   variable injected by Railway
-const pool = new pg.Pool();
 
 const app = express();
 const port = process.env.PORT || 3333;
 
-app.use(bodyParser.json());
-app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-app.use(bodyParser.text({ type: "text/html" }));
+app.use(express.json());
 
 app.get("/", async (req, res) => {
-  const { rows } = await pool.query("SELECT NOW()");
-  res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
+  res.send(`Hello world!`);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server is listening at http://localhost:${port}`);
 });
